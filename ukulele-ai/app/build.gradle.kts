dd3e995ae570580c846778352ade95d86dcae6f9
Edward Monteiro <edward.monteiro@gmail.com>
2026-09-10 17:50:59 -0300

    Version Ukulele AI M0.3

--- ukulele-ai/app/build.gradle.kts
+++ ukulele-ai/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.edward.ukuleleai"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.2.0-m0"
+        versionCode = 2
+        versionName = "0.3.0-m0"
     }
 
     buildFeatures {
