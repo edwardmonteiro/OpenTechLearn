9e2bac452e1bfe684da1d9a6c43b060dd670555b
Edward Monteiro <edward.monteiro@gmail.com>
2026-09-10 23:14:38 -0300

    Version Ukulele AI M0.4

--- ukulele-ai/app/build.gradle.kts
+++ ukulele-ai/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.edward.ukuleleai"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.3.0-m0"
+        versionCode = 3
+        versionName = "0.4.0-m0"
     }
 
     buildFeatures {
@@ -29,6 +29,7 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2025.05.00")
     implementation(composeBom)
 
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
     implementation("androidx.compose.material3:material3")
